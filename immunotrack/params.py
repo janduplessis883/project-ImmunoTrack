df29905163ee436bb0d8392285e97e1e649092dc
Jan du Plessis <drjanduplessis@icloud.com>
2024-03-05 17:36:15 +0000

    init

---
+++ immunotrack/params.py
@@ -0,0 +1,17 @@
+import os
+
+# Folder path for Automation - Git
+LOCAL_GIT_REPO = os.path.join(
+    os.path.expanduser("~"),
+    "code",
+    "janduplessis883",
+    "project-ImmunoTrack",
+)
+
+RAW_DATA = os.path.join(
+    os.path.expanduser("~"),
+    "code",
+    "janduplessis883",
+    "project-ImmunoTrack",
+    "data",
+)
